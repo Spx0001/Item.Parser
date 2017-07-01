e1f346b52fd6b383ef22e6901063b5c058491f86
Taniel Rémi <taniel.remi@gmail.com>
2017-07-01 10:37:47 +0200

    Dissiociate files and data folder

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Item.Parser
-Script PHP récuperant les infos des items depuis les langs de Dofus 1.29
+Script PHP récuperant les infos des items depuis les langs de Dofus 1.29.
 
 ## Utilisation
-Le script est prévu pour être lancé en mode console
+Le script est prévu pour être lancé en mode console, pour le lancer, éxécuter cette commande à la racine du dossier:
 ```
 php parser.php
 ```
 
-Les informations des langs doivent être contenus dans un fichier item.txt à la racine du dossier, le script générera les swf dans le dossier `output/`
+Les informations des langs doivent être contenus dans un fichier `item.txt` à la racine du dossier, le script générera les swf dans le dossier `files/`.
+
+Le script génère aussi un fichier contenant les erreurs rencontrés lors de son éxécution ainsi qu'un fichier JSON dans le dossier `data/`.
--- parser.php
+++ parser.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 if (!file_exists('item.txt'))
 {
-	exit("Unable to find item txt file.");
+	exit("Unable to find item txt file".PHP_EOL);
 }
 
 $data = file_get_contents('item.txt');
@@ -12,7 +12,14 @@ $errors = array();
 $items = array();
 
 // On vide le dossier de sortie
-foreach (glob('output/*') as $file)
+echo "Deleting older generated files (".count(glob('files/*.swf')).")".PHP_EOL;
+
+foreach (glob('files/*.swf') as $file)
+{
+	unlink($file);
+}
+
+foreach (glob('data/*') as $file)
 {
 	unlink($file);
 }
@@ -34,13 +41,15 @@ foreach (explode(';', $data) as $line)
 }
 
 // Copie des looks selon les ID des items
+echo "Trying to generate new SWF files (".count($items).")".PHP_EOL;
+
 foreach ($items as $item)
 {
 	$path = 'clips/items/'.$item['type'].'/'.$item['gfx'].'.swf';
 
 	if (file_exists($path))
 	{
-		if (!copy($path, 'output/'.$item['id'].'.swf'))
+		if (!copy($path, 'files/'.$item['id'].'.swf'))
 		{
 			array_push($errors, "Fail to generate SWF for item ".$item['id']);
 		}
@@ -51,10 +60,10 @@ foreach ($items as $item)
 	}
 }
 
-echo "Files successfully generated (".count($errors)." error(s) on ".count($items)." items)".PHP_EOL;
+echo "SWF files successfully generated (".count($errors)." error(s) on ".count($items)." items)".PHP_EOL;
 
 // Sauvegarde des erreurs dans un fichier texte
-$error_file = fopen('output/error.txt', 'w');
+$error_file = fopen('data/error.txt', 'w');
 
 foreach ($errors as $message)
 {
@@ -64,4 +73,6 @@ foreach ($errors as $message)
 fclose($error_file);
 
 // Enregistrement des données dans un fichier JSON
-file_put_contents('output/data.json', json_encode($items));
+file_put_contents('data/data.json', json_encode($items));
+
+echo "JSON file successfully generated".PHP_EOL;
